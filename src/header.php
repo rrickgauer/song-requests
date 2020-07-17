8284a2973e40f9e8a79967f26dba6a04a1717ad8
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-07-17 18:43:23 -0500

    add boxicons css

--- src/header.php
+++ src/header.php
@@ -2,5 +2,8 @@
 <!-- bootstrap -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
 
+<!-- boxicons -->
+<link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet' />
+
 <!-- personal css -->
 <link rel="stylesheet" type="text/css" href="css/style.css" />
