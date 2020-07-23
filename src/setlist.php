51cf4d064f759c5875b8d1afd2fc09730409d6e2
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-07-22 20:45:24 -0500

    create a setlist page for djs to view setlist info

---
+++ src/setlist.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include('functions.php');
+$setlist = getSetlistData($_GET['id'])->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include('header.php'); ?>
+  <title><?php echo $setlist['name']; ?></title>
+</head>
+<body>
+  <div class="container">
+
+    <h1 class="text-center mt-5"><?php echo $setlist['name']; ?></h1>
+    
+    
+  </div>
+
+  <?php include('footer.php'); ?>
+
+</body>
+</html>
