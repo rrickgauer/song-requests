4afc5aa715b96c74c91b31dd0fe06d318f854ec3
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-07-23 09:55:04 -0500

    add home page search bar

--- src/home.php
+++ src/home.php
@@ -8,7 +8,47 @@
 <body>
   <div class="container">
 
-    <h1 class="text-center mt-5">Song Requests</h1>
+    <h1 class="text-center mt-5 mb-5">Song Requests</h1>
+
+    <!-- search filter options -->
+    <div class="btn-group btn-group-toggle mb-3" data-toggle="buttons">
+      <label class="btn btn-secondary active">
+        <input type="radio" name="filter-option" value="all" checked> All
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="filter-option" value="dj"> DJ
+      </label>
+      <label class="btn btn-secondary">
+        <input type="radio" name="filter-option" value="setlist"> Setlist
+      </label>
+    </div>
+    
+    <!-- search bar input -->
+    <input type="text" class="form-control form-control-lg" placeholder="Type here to search">
+
+    <p class="text-center mt-3">Want to make your own setlist? <a href="login.php">Sign up</a></p>
+    
+    <!-- search results -->
+    <div class="search-results">
+      
+      <!-- djs -->
+      <div class="search-results-djs"></div>
+      
+
+      <!-- setlists -->
+      <div class="search-results-setlists"></div>
+
+      
+    </div>
+
+
+
+
+
+
+
+
+
 
     
   </div>
