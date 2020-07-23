08371ba0acc2fa942b7a7d0e32c92a307ed19963
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-07-22 21:11:44 -0500

    display setlist info as a table

--- src/setlist.php
+++ src/setlist.php
@@ -14,8 +14,61 @@ $setlist = getSetlistData($_GET['id'])->fetch(PDO::FETCH_ASSOC);
 <body>
   <?php include('navbar.php'); ?>
   <div class="container">
-    <h1 class="text-center mt-5"><?php echo $setlist['name']; ?></h1>
-    
+    <h1 class="text-center mt-3 mb-5"><?php echo $setlist['name']; ?></h1>
+
+    <h5 class="mb-3">Setlist data</h5>
+    <!-- setlist info table -->
+    <table class="table table-sm table-striped">
+      <tbody>
+        <tr>
+          <th>setlist name</th>
+          <td><?php echo $setlist['name']; ?></td>
+        </tr>
+
+        <tr>
+          <th>dj</th>
+          <td><?php echo $setlist['username']; ?></td>
+        </tr>
+
+        <tr>
+          <th>status</th>
+          <td><?php echo $setlist['status']; ?></td>
+        </tr>
+
+        <tr>
+          <th>time_start</th>
+          <td><?php echo $setlist['time_start']; ?></td>
+        </tr>
+
+        <tr>
+          <th>time_start_display_date</th>
+          <td><?php echo $setlist['time_start_display_date']; ?></td>
+        </tr>
+
+        <tr>
+          <th>time_start_display_time</th>
+          <td><?php echo $setlist['time_start_display_time']; ?></td>
+        </tr>
+
+        <tr>
+          <th>time_end</th>
+          <td><?php echo $setlist['time_end']; ?></td>
+        </tr>
+
+        <tr>
+          <th>time_end_display_date</th>
+          <td><?php echo $setlist['time_end_display_date']; ?></td>
+        </tr>
+
+        <tr>
+          <th>time_end_display_time</th>
+          <td><?php echo $setlist['time_end_display_time']; ?></td>
+        </tr>
+
+      </tbody>
+    </table>
+      
+
   </div>
 
   <?php include('footer.php'); ?>
