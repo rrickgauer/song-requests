0e74680002ed8f1e517d2bae4c4daf65dcc5b789
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-07-22 22:55:08 -0500

    move new setlist form into a modal

--- src/profile.php
+++ src/profile.php
@@ -39,76 +39,89 @@ if (isset($_POST['new-setlist-name'], $_POST['new-setlist-time-start'], $_POST['
   <div class="container">
     <h1 class="mt-3 mb-5"><?php echo $djInfo['username']; ?></h1>
 
-
     <div class="row">
       
+      <!-- create new setlist -->
+      <div class="col-sm-12 col-md-3">
+        <h5 class="mb-4">Actions</h5>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-new-setlist">Create</button>
+      </div>
+
       <!-- setlists -->
       <div class="col-sm-12 col-md-9 setlists">
         <h5>Your setlists</h5>
-        
         <!-- selist cards list -->
         <div id="setlists-list"></div>
 
       </div>
-      
-      <!-- create new setlist -->
-      <div class="col-sm-12 col-md-3">
-        <h5>New setlist</h5>
-        <form method="post">
-          <!-- setlist name -->
-          <div class="form-group">
-            <label for="new-setlist-name">Name:</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class='bx bx-list-ul'></i></span>
+    </div>
+  </div>
+
+  <!-- new setlist modal -->
+  <div class="modal modal-new-setlist fade " id="modal-new-setlist">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">New setlist</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form method="post">
+            <!-- setlist name -->
+            <div class="form-group">
+              <label for="new-setlist-name">Name:</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class='bx bx-list-ul'></i></span>
+                </div>
+                <input type="text" class="form-control new-setlist new-setlist-name" id="new-setlist-name" name="new-setlist-name" required>
               </div>
-              <input type="text" class="form-control new-setlist new-setlist-name" id="new-setlist-name" name="new-setlist-name" required>
             </div>
-          </div>
 
-          <!-- time start -->
-          <div class="form-group">
-            <label for="new-setlist-time-start">Start time:</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class='bx bx-calendar-plus'></i></span>
+            <!-- time start -->
+            <div class="form-group">
+              <label for="new-setlist-time-start">Start time:</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class='bx bx-calendar-plus'></i></span>
+                </div>
+                <input type="text" class="form-control new-setlist new-setlist-time new-setlist-time-start" id="new-setlist-time-start" name="new-setlist-time-start" required>
               </div>
-              <input type="text" class="form-control new-setlist new-setlist-time new-setlist-time-start" id="new-setlist-time-start" name="new-setlist-time-start" required>
             </div>
-          </div>
 
-
-          <!-- time end -->
-          <div class="form-group">
-            <label for="new-setlist-time-end">End time:</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class='bx bx-calendar-x'></i></span>
+            <!-- time end -->
+            <div class="form-group">
+              <label for="new-setlist-time-end">End time:</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class='bx bx-calendar-x'></i></span>
+                </div>
+                <input type="text" class="form-control new-setlist new-setlist-time new-setlist-time-end" id="new-setlist-time-end" name="new-setlist-time-end" required>
               </div>
-              <input type="text" class="form-control new-setlist new-setlist-time new-setlist-time-end" id="new-setlist-time-end" name="new-setlist-time-end" required>
             </div>
-          </div>
 
-          <!-- status -->
-          <div class="form-group">
-            <label for="new-setlist-time-end">Status:</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class='bx bx-stats'></i></span>
+            <!-- status -->
+            <div class="form-group">
+              <label for="new-setlist-time-end">Status:</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class='bx bx-stats'></i></span>
+                </div>
+                <select class="form-control new-setlist new-setlist-status" name="new-setlist-status">
+                  <option value="open" selected>Open</option>
+                  <option value="closed">Closed</option>
+                  <option value="paused">Paused</option>            
+                </select>
               </div>
-              <select class="form-control new-setlist new-setlist-status" name="new-setlist-status">
-                <option value="open" selected>Open</option>
-                <option value="closed">Closed</option>
-                <option value="paused">Paused</option>            
-              </select>
             </div>
-          </div>
 
-          <input type="submit" class="btn btn-primary float-right" value="Create">
-        </form>
+            <input type="submit" class="btn btn-primary float-right" value="Create">
+          </form>
+        </div>
       </div>
     </div>
-
   </div>
 
 
