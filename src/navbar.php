65b14024897b0c6f6e516901bc6110618a274775
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-07-27 19:37:53 -0500

    add logout.php

--- src/navbar.php
+++ src/navbar.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" id="nav-item-profile" href="profile.php">Profile</a></li>
-        <li class="nav-item"><a class="nav-link" id="nav-item-logout" href="login.php">Logout</a></li>
+        <li class="nav-item"><a class="nav-link" id="nav-item-logout" href="logout.php">Logout</a></li>
       </ul>
     </div>
   </nav>
