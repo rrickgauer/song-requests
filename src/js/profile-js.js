3f2977a401355c622bc18a556821af7d0596cc84
Ryan Rickgauer <rmr@premierintl.local>
2020-08-06 22:47:44 -0500

    add css to cards

--- src/js/profile-js.js
+++ src/js/profile-js.js
@@ -51,34 +51,25 @@ function getSetlistCardHtml(setlist) {
   var html = '';
   html += '<div class="card card-setlist" data-setlist-id="' + setlist.id + '">';
   html += '<div class="card-body">';
-  html += '<h4 class="card-title">' + setlist.name + '</h4>';
-
-  // start time
-  html += '<p class="card-text">Starts: ';
-  html += setlist.time_start_display_date + ' at ';
-  html += setlist.time_start_display_time + '</p>';
-
-  //end time
-  html += '<p class="card-text">Ends: ';
-  html += setlist.time_end_display_date + ' at ';
-  html += setlist.time_end_display_time + '</p>';
-
-  html += '<p class="card-text">Requests: ' + setlist.count_requests + '</p>'; 
-
+  html += '<p class="card-title"><b>' + setlist.name + '</b>';
+  html += '<span class="badge badge-info ml-2">' + setlist.count_requests + '</span></p>';
+  html += '<p class="card-text card-text-dates">';
+  html += setlist.time_start_display_date + ' - ';
+  html += setlist.time_end_display_date + '</p>';
   html += '</div>';
+  
   html += '<div class="card-footer">';
 
   // status
   if (setlist.status == 'open')
-    html += '<span class="badge badge-success">' + setlist.status + '</span>';
+    html += '<span class="badge badge-pill badge-success">' + setlist.status + '</span>';
   else if (setlist.status == 'closed')
-    html += '<span class="badge badge-danger">' + setlist.status + '</span>';
+    html += '<span class="badge badge-pill badge-danger">' + setlist.status + '</span>';
   else
-    html += '<span class="badge badge-warning">' + setlist.status + '</span>';
+    html += '<span class="badge badge-pill badge-warning">' + setlist.status + '</span>';
 
   // link to setlist page
   html += '<a href="dj-setlist.php?id=' + setlist.id + '" class="float-right">Details</a>';
-
   html += '</div>';
   html += '</div> ';
 
